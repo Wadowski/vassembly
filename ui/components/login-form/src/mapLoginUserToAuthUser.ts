616908e02f41972e45223065cb0fe247c51c28e0
Wadowski <domwadowski@gmail.com>
2026-04-30 22:58:11 +0200

    fix field after login

--- ui/components/login-form/src/mapLoginUserToAuthUser.ts
+++ ui/components/login-form/src/mapLoginUserToAuthUser.ts
@@ -1,6 +1,6 @@
 import type { AuthUser, MapLoginUserToAuthUserParams } from './types';
 
 export const mapLoginUserToAuthUser = (params: MapLoginUserToAuthUserParams): AuthUser => {
-  const { id, email } = params;
-  return { id, email };
+  const { id, email, firstName, lastName } = params;
+  return { id, email, firstName, lastName };
 };
