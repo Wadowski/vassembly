8a1ac5768e7640347422d9f083b77cb638048f3f
Wadowski <domwadowski@gmail.com>
2026-05-02 22:34:55 +0200

    fix build

--- ui/api-hooks/src/auth/index.ts
+++ ui/api-hooks/src/auth/index.ts
@@ -1,6 +1,6 @@
-export { useForgotPassword } from './useForgotPassword';
+export { useForgotPassword, type ForgotPasswordResponse } from './useForgotPassword';
 export { useLogin } from './useLogin';
-export { useRegister } from './useRegister';
+export { useRegister, type RegisterResponse } from './useRegister';
 export { useRefresh } from './useRefresh';
 export { useAuth } from './useAuth';
 export { useLogout } from './useLogout';
