0acc45ed304f907f60d1ad3fc8cac1bc5870e9b9
Wadowski <domwadowski@gmail.com>
2026-06-17 16:23:11 +0200

    remove console.log

--- packages/client-mongodb/src/connection.ts
+++ packages/client-mongodb/src/connection.ts
@@ -6,7 +6,6 @@ export const MongoDbConnection = ({
   url = config.mongoDb.url,
   dbName = config.mongoDb.database,
 } = {}) => {
-  console.log('url', url);
   const client = new MongoClient(url);
   const db = client.db(dbName);
 
